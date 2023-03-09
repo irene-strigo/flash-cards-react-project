6818b513c8809c0df32c7330f6578137a9e4ba18
Irene Strigo <irene.strigo@gmail.com>
2023-03-09 07:05:44 +0100

    card changes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useContext } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { VoiceComponent } from '../VoiceComponent/VoiceComponent'
 /*import { ThemeContext } from "../Context/Context";*/
 
