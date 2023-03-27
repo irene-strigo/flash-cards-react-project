cd9aef5582e2a2055bd61df27f15fd03ffd2180a
Irene Strigo <irene.strigo@gmail.com>
2023-03-27 20:30:55 +0200

    hover on header buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,14 +9,14 @@ function Header() {
         <>
             <div className='mainMenuContainer'>
                 <div className='Heading'>Флеш-карты для изучения английского языка</div>
-                <div>Test div</div>
+
                 <nav className='mainMenu'>
 
-                    <Link className="navLink" to="/"><img src="assets/images/home.png" alt='home'></img></Link>
+                    <Link className="navLink" to="/"><img className="navPicture" src="assets/images/home.png" alt='home'></img></Link>
 
-                    <Link className="navLink" to="/game"><img src="assets/images/cards.png" alt='game'></img></Link>
+                    <Link className="navLink" to="/game"><img className="navPicture" src="assets/images/cards.png" alt='game'></img></Link>
 
-                    <Link className="navLink" to="/cards"><img src="assets/images/container.png" alt='home'></img></Link>
+                    <Link className="navLink" to="/cards"><img className="navPicture" src="assets/images/container.png" alt='home'></img></Link>
 
                 </nav></div>
 
